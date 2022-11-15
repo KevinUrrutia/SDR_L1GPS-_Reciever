dc59ac6a414cb6e83f317e57acb54b4430012dc1
Kevin Urrutia <kevinurrutia@pop-os.localdomain>
2022-11-14 19:09:05 -0800

    added acquisition data

---
+++ Reciever/acquisition/acquisition.m
@@ -0,0 +1,46 @@
+clear; clc; close all;
+
+%----Setup
+dur = 0.5; %time interval of data to load
+fsamp = 5e6; %IQ sampling frequency
+Tc = 1e-3; %code period [s]
+fchip = 1.023e6; %chipping rate [Hz]
+del_chip = 3/217; % sampling interval in chips 
+delt = del_chip * Tc; % sampling interval in seconds
+fdmin = -6000; %minimum Doppler Frequency to search
+fdmax = 6000; %maximum Doppler Frequency to search
+deltafd = 500; %doppler resolution to search
+
+%---Generate PRN code bank
+PRN = createPRNBank();
+
+%---Load Data
+[I , Q] = IQ_parsing('niData01head.bin');
+X = I + 1j*Q;
+
+%---load over accumulation period
+Ns = fsamp * Tc; %you get the number of samples in one code period
+Xp = X(1: Ns); 
+
+%--oversamples the code 
+C = cell([1, 34]);
+for i = 1:34
+    C{i} = resample(PRN{i}, fsamp, fchip); %interpolate the PRN code to the chipping rate
+end
+
+%---plot setup
+fdVec = fdmin:deltafd:fdmax;
+startTimeVec = 0:length(C{1})-1;
+[Dopplerm, timem] = meshgrid(fdVec, startTimeVec/fsamp);
+
+%--calculate N0 (noise denisity), can done by doing the autocorrelation with an unusused C/A code
+[N0, SNk2] = calcN0(fdmax, fdmin, deltafd, Ns, fsamp, C{34}, Xp);
+display("Calculated N0: " + N0);
+
+
+%--PRN search
+SK2 = acq_svid(fdmax, fdmin, Ns, deltafd, fsamp, C, Xp);
+
+%--Plot PRN
+[ts_hat, fd_hat, C_N0] = plotPRN(SK2, Tc, fsamp, N0, startTimeVec, fdVec, Dopplerm, timem);
+
---
+++ Reciever/acquisition/plotPRN.m
@@ -0,0 +1,28 @@
+function [ts_hat, fd_hat, C_N0] = plotPRN(SK2, Tc, fsamp, N0, startTimeVec, fdVec, Dopplerm, timem)
+    %inputs: SK2: Power spectra of each satellite
+    %        Tc: chipping period
+    %        fsamp: sampling rate
+    %        N0: noise desity
+
+    %outputs: ts_hat: code estimate
+    %         fd_hat: doppler est
+    %         C_N0: carrier to noise ratio
+ 
+    ts_hat = zeros(1, 8);
+    fd_hat = zeros(1, 8);
+    C_N0 = zeros(1, 8);
+
+    for i = [1 2 5 10 12 15 29 30]
+         [maxSk2Vec,maxSk2indVec] = max(SK2{i});
+         [maxSk2, ind_fd_hat] = max(maxSk2Vec);
+         ind_ts_hat = maxSk2indVec(ind_fd_hat);
+         ts_hat(i) = mod(startTimeVec(ind_ts_hat),floor(Tc*fsamp))/fsamp;
+         fd_hat(i) = fdVec(ind_fd_hat);
+         C_N0(i) = 10*log10((maxSk2-N0)/(N0*Tc));
+         figure;
+         surf(Dopplerm,timem,SK2{i})
+         xlabel('f_d [Hz]')
+         ylabel('t_s [sec]')
+         title(strcat(['|S_k|^2 for PRN ' int2str(i)])) 
+    end
+end
